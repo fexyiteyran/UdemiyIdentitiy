cc8008ea2cbb9fcee7f2c88e194c4c97c1c7017c
alieryaman <postauyelik@gmail.com>
2020-06-08 22:35:17 +0300

    Identity ayarlari yapildi

---
+++ UdemiyIdentitiy/Context/UdemiyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemiyIdentitiy.Context
+{
+    public class UdemiyContext:IdentityDbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer(@"server=DESKTOP-NV0GSA0\SQLEXPRESS;database=udemyBlogToDo");
+            optionsBuilder.UseSqlServer(@"Server = DESKTOP-NV0GSA0\SQLEXPRESS; Database = UdemyIdentity; Trusted_Connection = True; MultipleActiveResultSets = true");
+        }
+
+
+
+
+    }
+}
