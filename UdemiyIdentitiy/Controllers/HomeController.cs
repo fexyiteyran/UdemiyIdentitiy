840a00a93156e07b7f425e833e8427bd9eb44885
alieryaman <postauyelik@gmail.com>
2020-06-09 22:37:41 +0300

    sign islemleri

--- UdemiyIdentitiy/Controllers/HomeController.cs
+++ UdemiyIdentitiy/Controllers/HomeController.cs
@@ -2,25 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using UdemiyIdentitiy.Context;
 using UdemiyIdentitiy.Models;
 
 namespace UdemiyIdentitiy.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly UserManager<AppUser> _usermanager;
+        public HomeController(UserManager<AppUser> usermanager)
+        {
+            usermanager = _usermanager;
+        }
+
+
         public IActionResult Index()
         {
             return View(new UserSignInViewModel());
         }
+        public IActionResult GirisYap(UserSignInViewModel model)
+        {
 
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View("Index", model);
+        }
 
         public IActionResult KayitOl()
         {
             return View(new UserSignUpViewModel());
         }
+        [HttpPost]
+        
 
 
+       [HttpPost]   
+        public  async Task<IActionResult> KayitOl(UserSignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    Email = model.Email,
+                    Name=model.Name,
+                    SurName=model.SurName,
+                    UserName=model.UserName
+
+                };
+
+            
+           var result=    await  _usermanager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+
+            }
+          
+
+
+            return View(model);
+        }
 
 
 
--- UdemiyIdentitiy/Models/UserSignInViewModel.cs
+++ UdemiyIdentitiy/Models/UserSignInViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace UdemiyIdentitiy.Models
 {
     public class UserSignInViewModel
     {
-
+        [Display(Name ="Kullanıcı Adı")]
+        [Required(ErrorMessage ="kullanıcı aı boş geçemez")]
         public string UserName { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre alanı boş geçemez")]
         public string Password { get; set; }
     }
 }
