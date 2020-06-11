30d75b29a14f0c913cbc679b5e121e80e84162c8
alieryaman <postauyelik@gmail.com>
2020-06-12 00:58:14 +0300

    Kullanıcı güncellme resim yükleme işlemleri

--- UdemiyIdentitiy/Controllers/PanelController.cs
+++ UdemiyIdentitiy/Controllers/PanelController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using UdemiyIdentitiy.Context;
+using UdemiyIdentitiy.Models;
 
 namespace UdemiyIdentitiy.Controllers
 {
@@ -25,6 +28,62 @@ namespace UdemiyIdentitiy.Controllers
            var user=await  _userManager.FindByNameAsync(User.Identity.Name);
             return View(user);
         }
+
+        public async Task<IActionResult> UpdateUser()
+        {
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserUpdateViewModel model = new UserUpdateViewModel
+            {
+                Email=user.Email,
+                Name=user.Name,
+                SurName = user.SurName,
+               PhoneNumber=user.PhoneNumber,
+               PuctureUrl=user.PictureUrl
+            };
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser(UserUpdateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                if (model.Picture !=null)
+                {
+                    var uygulamaninCalistigiYer = Directory.GetCurrentDirectory();
+                    var uzanti = Path.GetExtension(model.Picture.FileName);
+                    var resimad = Guid.NewGuid() + uzanti;
+                    var kaydedilecekYer = uygulamaninCalistigiYer + "/wwwroot/img/" + resimad;
+                    using var stream = new FileStream(kaydedilecekYer, FileMode.Create);
+                    await model.Picture.CopyToAsync(stream);
+                    user.PictureUrl = resimad;
+                    
+
+                }
+
+                user.Name = model.Name;
+                user.SurName = model.SurName;
+                user.Email = model.Email;
+                user.PhoneNumber = model.PhoneNumber;
+               
+               var result= await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
+        }
+
+
         //herkes erişsin istiyorsak bunu yazarız
         [AllowAnonymous]
         public IActionResult Herkeserisin()
---
+++ UdemiyIdentitiy/Models/UserUpdateViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemiyIdentitiy.Models
+{
+    public class UserUpdateViewModel
+    {
+        [Display(Name="Email")]
+        [Required(ErrorMessage ="Email alanı gereklidir")]
+        [EmailAddress(ErrorMessage ="Lütfen geçerli bir email adresini giriniz")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Telefon:")]
+        public string PhoneNumber { get; set; }
+
+        public string PuctureUrl { get; set; }
+        public IFormFile Picture { get; set; }
+       
+        [Display(Name = "Adı:")]
+        [Required(ErrorMessage = "Name alanı gereklidir")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname alanı gereklidir")]
+        [Display(Name = "Soyisim:")]
+        public string SurName { get; set; }
+    }
+}
