c342cd4d8e751081b3f7e5e048210da56bf2e1ea
alieryaman <postauyelik@gmail.com>
2020-06-11 17:53:16 +0300

    User layout olusturma

--- UdemiyIdentitiy/Controllers/PanelController.cs
+++ UdemiyIdentitiy/Controllers/PanelController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using UdemiyIdentitiy.Context;
 
 namespace UdemiyIdentitiy.Controllers
 {
@@ -11,7 +13,19 @@ namespace UdemiyIdentitiy.Controllers
       [Authorize]
     public class PanelController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public PanelController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+        //herkes erişsin istiyorsak bunu yazarız
+        [AllowAnonymous]
+        public IActionResult Herkeserisin()
         {
             return View();
         }
