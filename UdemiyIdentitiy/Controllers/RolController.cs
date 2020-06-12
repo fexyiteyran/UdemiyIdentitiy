500cbd49f08dbcb5e7b0c18593c3cadf8ece1235
alieryaman <postauyelik@gmail.com>
2020-06-12 22:01:26 +0300

    Role eklme işlemi yapıldı

---
+++ UdemiyIdentitiy/Controllers/RolController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemiyIdentitiy.Context;
+using UdemiyIdentitiy.Models;
+
+namespace UdemiyIdentitiy.Controllers
+{
+
+    [Authorize]
+    public class RolController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolController(RoleManager<AppRole> roleManager)
+        {
+               _roleManager=roleManager; 
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(_roleManager.Roles.ToList());
+        }
+
+        public IActionResult AddRole()
+        {
+
+            return View(new RoleViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = model.Name
+                };
+                var identityResult = await _roleManager.CreateAsync(role);
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ UdemiyIdentitiy/Models/RoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemiyIdentitiy.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Ad Alanı gerej-klidir")]
+        [Display(Name ="Ad:")]
+        public string Name { get; set; }
+    }
+}
